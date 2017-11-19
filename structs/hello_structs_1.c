46267ba30b0135630eac8790848a211f8b9f4032
jverl <johanveramendi@gmail.com>
2017-11-19 18:11:37 -0500

    MakeFile

---
+++ structs/hello_structs_1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct complex
+{
+	int re;
+	int im;
+};
+
+#define complexi struc complex
+complexi addcomplex(complexi, complexi)
+int main(void){
+	complexi z;
+	z.re = 1;
+	z.im = 2;
+
+	complexi w;
+	z.re = 1;
+	z.im = 2;
+
+	complexi x;
+	z.re = 1;
+	z.im = 2;
+
+
+
+
+
+	printf("z=%i+%i",z.re,z.im);
+	printf("w=%i+%i",w.re,w.im);
+	printf("x=%i+%i",x.re,x.im);
+
+	return EXIT_SUCCESS;
+}
+
+complexi addcomplex(complexi z1, complexi z2){
+	complexi result;
+	result.re = z1.re +z2.re;
+	result.im = z1.im + z2.im;
+	return result;
+}
